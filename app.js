537399d29e81f8d90347042d783329456f996b6d
Filip de Mesmaeker <49500680+FilipDM89@users.noreply.github.com>
2019-11-13 14:29:30 +0100

    make articels viewable in history page

--- app.js
+++ app.js
@@ -7,15 +7,32 @@ const port          = process.env.PORT || 1837;
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+//test history articles
+
+const historyArticles = [
+    {title: "Gatling guns and their impact", image: "https://bit.ly/2NKkjSj", content: "Rapid firing changed the way war was fought drastically forever"},
+    {title: "The Slow decline of Cavalry", image:"https://i.pinimg.com/originals/9f/9a/f5/9f9af56734829adc63fca95ef65b6f07.jpg", content:"It took very long for Cavalry to be disbanded as a traditional component of a standing army."},
+    {title: "African-American regiments during the Civil War", image:"https://bit.ly/2NK90JV", content: "The abolishment of slavery sadly did not mean the immediate disappearance of racism."}
+]
+
 //ROUTES
 
 app.get("/", (req, res)=> {
- res.render("landing")
-})
+ res.render("landing");
+});
 
 app.get("/history", (req, res)=> {
-    res.render("history")
-   })
+    res.render("history", {historyArticles: historyArticles})
+   });
+
+app.post("/history", (req, res) =>{
+    let title = req.body.title;
+    let image = req.body.image;
+    let content = req.body.content;
+    let newHistoryArticle = {title: title, image: image, content: content};
+    historyArticles.push(newHistoryArticle);
+    res.redirect("/history")
+});
 
 //PORT LISTEN
 app.listen(port, function(){
