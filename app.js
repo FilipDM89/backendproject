ad690acfc4d2dd2e8958d5da6b04e9ae292df64d
Filip de Mesmaeker <49500680+FilipDM89@users.noreply.github.com>
2019-11-13 11:54:57 +0100

    Add navbar and footer/header

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res)=> {
  res.render("landing")
-})
+});
+
+app.get("/history", (req, res)=> {
+    res.render("landing")
+   })
 
 //PORT LISTEN
 app.listen(port, function(){
