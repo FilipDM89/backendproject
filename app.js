20dee9b13ea84b6b59ce88aa73a2f1b1b496ab7d
Filip de Mesmaeker <49500680+FilipDM89@users.noreply.github.com>
2019-11-14 13:56:46 +0100

    added schema and test article

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose      = require("mongoose")
 const port          = process.env.PORT || 1837;
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.set(express.static("public"));
 app.set("view engine", "ejs");
 
 mongoose.connect("mongodb://localhost/aoi", {
@@ -34,6 +35,26 @@ app.get("/components/infantry", (req, res)=> {
 
 app.get("/components/cavalry", (req, res)=> {
     res.render("components/cavalry")
+});
+
+
+//SCHEMA
+
+const historyArticleSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	content: String,
+	created: {
+		type: Date, 
+		default: Date.now
+	}
+});
+
+const historyArticle = mongoose.model("historyArticle", historyArticleSchema)
+historyArticle.create({
+	title: "test1",
+	image: "https://i.pinimg.com/originals/a9/5d/ed/a95ded0add7dea7ec7484d75c554a1a8.jpg",
+	content: "testing the content right here",
 });
 
 //PORT LISTEN
