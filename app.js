f138b333299fb3ce347769c86483485a4fa89d2c
Filip de Mesmaeker <49500680+FilipDM89@users.noreply.github.com>
2019-12-10 14:36:48 +0100

    first attempt pagination

--- app.js
+++ app.js
@@ -1,61 +1,65 @@
 //SETUP
-const express       = require("express");
-const app           = express();
-const bodyParser    = require("body-parser");
-const mongoose      = require("mongoose");
-const methodOverride= require("method-override");
-const historyArticle= require("./models/historyarticle");
-const commentArticle= require("./models/comment.js");
-const user 			= require("./models/user"); 
-const passport		= require("passport") //necessary for auth
-const localStrategy = require("passport-local") //necessary for auth
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const historyArticle = require("./models/historyarticle");
+const commentArticle = require("./models/comment.js");
+const user = require("./models/user");
+const passport = require("passport"); //necessary for auth
+const localStrategy = require("passport-local"); //necessary for auth
 const passportLocal = require("passport-local-mongoose");
-const historyRoutes = require("./routes/historyarticles")
-const indexRoutes	= require("./routes/index")
-const commentRoutes	= require("./routes/comments")
-const port          = process.env.PORT || 1837;
+// const escapeRegex = require("./middleware/regex-escape");
+const historyRoutes = require("./routes/historyarticles");
+const indexRoutes = require("./routes/index");
+const commentRoutes = require("./routes/comments");
+const port = process.env.PORT || 1837;
 
+mongoose
+  .connect("mongodb://localhost/aoi", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log("Connected to DB!");
+  })
+  .catch(err => {
+    console.log("ERROR:", err.message);
+  });
 
-
-mongoose.connect("mongodb://localhost/aoi", {
-	useUnifiedTopology: true,
-	useNewUrlParser: true,
-	useCreateIndex: true
-}).then(() => {
-	console.log("Connected to DB!");
-}).catch(err => {
-	console.log("ERROR:", err.message);
-});
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-app.use(require("express-session")({
-	secret: "Rule Britannia",
-	resave: false,
-	saveUninitialized: false
-})); //needs to above
+app.use(
+  require("express-session")({
+    secret: "Rule Britannia",
+    resave: false,
+    saveUninitialized: false
+  })
+); //needs to above
 
 app.use(passport.initialize()); //part of auth
 app.use(passport.session()); //part of auth
-
 
 passport.use(new localStrategy(user.authenticate()));
 passport.serializeUser(user.serializeUser()); //part of auth
 passport.deserializeUser(user.deserializeUser()); //part of auth
 
- //part of auth
+//part of auth
 
 app.use((req, res, next) => {
-	res.locals.currentUser = req.user;
-    next();
+  res.locals.currentUser = req.user;
+  next();
 }); //tells all pages currentuser = user
 
 app.use(historyRoutes);
 app.use(indexRoutes);
 app.use(commentRoutes);
+// app.use(escapeRegex);
 
 // app.use("/history", historyRoutes); a way to dry the code, later on
 
@@ -63,28 +67,26 @@ app.use(commentRoutes);
 //ROUTES
 //=========
 
-app.get("/", (req, res)=> {
- res.render("index");
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-
-app.get("/weaponry/firearms", (req, res)=> {
-	res.render("weaponry/firearms");
-   });
-
-app.get("/components/infantry", (req, res)=> {
-    res.render("components/infantry")
+app.get("/weaponry/firearms", (req, res) => {
+  res.render("weaponry/firearms");
 });
 
-app.get("/components/cavalry", (req, res)=> {
-    res.render("components/cavalry")
+app.get("/components/infantry", (req, res) => {
+  res.render("components/infantry");
 });
 
+app.get("/components/cavalry", (req, res) => {
+  res.render("components/cavalry");
+});
 
 //===========
 //PORT LISTEN
 //===========
 
-app.listen(port, function(){
-    console.log("Age of Imperialism server has started!")
+app.listen(port, function() {
+  console.log("Age of Imperialism server has started!");
 });
--- routes/historyarticles.js
+++ routes/historyarticles.js
@@ -1,53 +1,83 @@
-const express 		= require("express");
-const router  		= express.Router({mergeParams: true});
-const historyArticle= require("../models/historyarticle");
-
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const historyArticle = require("../models/historyarticle");
 
 //Retrieves articles from DB
-router.get("/history", (req, res)=> {
-    //retrieves articles from the database
-     historyArticle.find({}, (err, historyarticles) => {
-         if(err){
-             console.log(err)
-         } else {
-			 res.render("history", {historyarticles: historyarticles, currentUser: req.user})
-         };
-     });
- });
+router.get("/history", (req, res) => {
+  //retrieves articles from the database
+  historyArticle.find({}, function(err, historyarticles) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("history", {
+        historyarticles: historyarticles,
+        currentUser: req.user
+      });
+    }
+  });
+});
+
+// router.get("/history/:page", async (req, res, next) => {
+//   const resPerPage = 6;
+//   const page = 1;
+//   try {
+//     if (req.query.search) {
+//       const searchQuery = req.query.search,
+//         regex = new RegExp(escapeRegex(req.query.search), "gi");
+//       const foundArticles = await historyArticle
+//         .find({ name: regex })
+//         .skip(resPerPage * page - resPerPage)
+//         .limit(resPerPage);
+//       const numOfArticles = await historyArticle.count({ name: regex });
+//       res.render("history"),
+//         {
+//           articles: foundArticles,
+//           currentPage: page,
+//           pages: Math.ceil(numOfArticles / resPerPage),
+//           searchVal: searchQuery,
+//           numOfResults: numOfArticles
+//         };
+//     }
+//   } catch {
+//     throw new Error(err);
+//   }
+// });
 
 //=============
 //CREATE ROUTE
 //=============
 
 router.get("/history/new", (req, res) => {
-	res.render("new")
+  res.render("new");
 }); //has to be above the post statement
-
- router.post("/history", (req, res) => {
-	//creates new article
-	historyArticle.create(req.body.historyarticle, (err, newHistoryArticle) => {
-		if(err){
-			console.log(err);
-		} else {
-			//if made succesfully, it is sent to history
-			res.redirect("/history")
-		}
-	})
+router.post("/history", (req, res) => {
+  //creates new article
+  historyArticle.create(req.body.historyarticle, (err, newHistoryArticle) => {
+    if (err) {
+      console.log(err);
+    } else {
+      //if made succesfully, it is sent to history
+      res.redirect("/history");
+    }
+  });
 });
 
 //==========
 //SHOW ROUTE
 //==========
 
-router.get("/history/:id", (req, res) =>{
-	//finds article by its id number first
-	historyArticle.findById(req.params.id).populate("comments").exec((err, foundArticle) => {
-		if(err){
-			res.render("history");
-		} else {
-			res.render("show", {historyarticle: foundArticle});
-		}
-	});
+router.get("/history/:id", (req, res) => {
+  //finds article by its id number first
+  historyArticle
+    .findById(req.params.id)
+    .populate("comments")
+    .exec((err, foundArticle) => {
+      if (err) {
+        res.render("history");
+      } else {
+        res.render("show", { historyarticle: foundArticle });
+      }
+    });
 });
 
 //==========
@@ -55,45 +85,47 @@ router.get("/history/:id", (req, res) =>{
 //==========
 
 router.get("/history/:id/edit", (req, res) => {
-	historyArticle.findById(req.params.id, (err, foundArticle) => {
-		if(err){
-			res.redirect("history");
-		} else {
-			res.render("edit", {historyarticle: foundArticle});
-		}
-	});
+  historyArticle.findById(req.params.id, (err, foundArticle) => {
+    if (err) {
+      res.redirect("history");
+    } else {
+      res.render("edit", { historyarticle: foundArticle });
+    }
+  });
 });
- 
 
- //============
+//============
 //UPDATE ROUTE
 //============
 
-
- router.put("/history/:id", (req, res) => {
-	historyArticle.findByIdAndUpdate(req.params.id, req.body.historyarticle, (err, editedArticle) => {
-		if(err){
-			res.redirect("history");
-		} else {
-			res.redirect("/history/" + req.params.id);
-		}
-	});
+router.put("/history/:id", (req, res) => {
+  historyArticle.findByIdAndUpdate(
+    req.params.id,
+    req.body.historyarticle,
+    (err, editedArticle) => {
+      if (err) {
+        res.redirect("history");
+      } else {
+        res.redirect("/history/" + req.params.id);
+      }
+    }
+  );
 });
 
 //============
 //DELETE ROUTE
-//============ 
+//============
 
 router.delete("/history/:id", (req, res) => {
-	//destroy blog
-	historyArticle.findByIdAndRemove(req.params.id, (err) => {
-		if(err){
-			console.log(err)
-		} else {
-			//redirect somewhere
-			res.redirect("/history")
-		}
-	});
+  //destroy blog
+  historyArticle.findByIdAndRemove(req.params.id, err => {
+    if (err) {
+      console.log(err);
+    } else {
+      //redirect somewhere
+      res.redirect("/history");
+    }
+  });
 });
 
 module.exports = router;
